7d464415c15bf59bb2601e0ce68016fb6e6b6991
Evan Jam <evan.jam94@gmail.com>
2026-02-25 16:39:39 -0500

    test new program structure (run_query) with hardcoded sql query

--- main.py
+++ main.py
@@ -2,21 +2,55 @@ import os
 import pyodbc
 from google import genai
 
+#----------
 #Database Layer
+#----------
 
 def run_query(sql: str) -> list[dict]:
     # connect
     # execute
     # return rows as list of dicts
     # mostly in the dbtest.py proof of concept
-    pass
+    DRIVER_NAME = "ODBC Driver 18 for SQL Server"
+    SERVER = os.getenv("NorthwindSVR")
+    DATABASE = os.getenv("NorthwindDB")
+    USER = os.getenv("NorthwindUID")
+    PASSWORD = os.getenv("NorthwindPWD")
+
+    if not all([SERVER, DATABASE, USER, PASSWORD]):
+        raise RuntimeError("Database environment variables are not set properly.")
+    
+    conn_str = (
+    f"DRIVER={{{DRIVER_NAME}}};"
+    f"SERVER={SERVER};"
+    f"DATABASE={DATABASE};"
+    f"UID={USER};"
+    f"PWD={PASSWORD};"
+    "Encrypt=yes;"
+    "TrustServerCertificate=yes;"
+)
+    
+    with pyodbc.connect(conn_str) as conn:
+        cur = conn.cursor()
+        cur.execute(sql)
+
+        columns = [column[0] for column in cur.description]
+        rows = cur.fetchall()
+
+        results = []
+        for row in rows:
+            results.append(dict(zip(columns, row)))
+
+        return results
 
 def list_tables() -> list[dict]:
     # hardcoded SQL
     # call run_query()
     pass
 
+#----------
 #Gemini Layer
+#----------
 
 SYSTEM_INSTRUCTIONS = """
 You are a database assistant.
@@ -30,7 +64,9 @@ def create_chat():
     # evolved version of phase0's "prompt_gemini" function that's used to instantiate persistent chats instead of sending one-off 1:1 prompt->response
     pass
 
+#----------
 #Entry Point
+#----------
 
 def main():
     chat = create_chat()
@@ -42,3 +78,14 @@ def main():
 
         response = chat.send_message(user_input)
         print(response.text)
+
+#TEMP TEST BLOCK
+if __name__ == "__main__":
+    data = run_query("""
+        SELECT TOP (5)
+            EmployeeID,
+            FirstName,
+            LastName
+        FROM dbo.Employees
+    """)
+    print(data)
